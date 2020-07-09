89f9929e5b27af4787df27163cbfb53d590547e4
Karsten Taylor <karstentaylor0@gmail.com>
2020-07-09 13:43:15 -0700

    adding ID

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,23 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+const users = [
+  {
+    id: '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+    username: 'sallyStudent',
+    password: 'c00d1ng1sc00l',
+    favoriteClub: 'Cache Valley Stone Society',
+    newsLetter: 'true',
+  },
+  {
+    id: 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+    username: 'johnBlocton',
+    password: 'veryg00dpassw0rd',
+    favoriteClub: 'Salt City Curling Club',
+    newsLetter: 'false',
+  },
+];
 
 app.post('/user', (req, res) => {
   //get the data
@@ -49,6 +67,17 @@ app.post('/user', (req, res) => {
   if (!clubs.includes(favoriteClub)) {
     return res.status(400).send('Not a valid club');
   }
+
+  const id = uuid();
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter,
+  };
+  users.push(newUser);
+
   res.send('All validation passed');
 });
 
